5ffbbbe1ee9234ae6898c1fddd72cc2dc018fd95
joseapp25 <joseapp25@gmail.com>
2025-04-03 15:26:31 -0300

    youtube link test

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ function toggleTranslation(id) {
     var translation = document.getElementById(id);
     translation.classList.toggle("show");
 }
+
+function getYouTubeID(url) {
+    const regExp = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
+    const match = url.match(regExp);
+    return match ? match[1] : null;
+}
+
+function setYouTubeThumbnails() {
+    const videoLinks = document.querySelectorAll(".video-link");
+
+    videoLinks.forEach(link => {
+        const videoURL = link.href;
+        const videoID = getYouTubeID(videoURL);
+        const img = link.querySelector(".video-thumbnail");
+
+        if (videoID) {
+            img.src = `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`;
+        } else {
+            console.error("Invalid YouTube URL:", videoURL);
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", setYouTubeThumbnails);
