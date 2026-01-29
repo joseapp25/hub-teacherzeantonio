219e28675fedeea8a38e829bc923e924d37e8817
joseapp25 <joseapp25@gmail.com>
2026-01-29 15:41:08 -0300

    ad

--- component-global-menu.js
+++ component-global-menu.js
@@ -6,11 +6,11 @@ class ComponentGlobalMenu extends HTMLElement{
 
             <link rel="stylesheet" href="style.css">
 
-            <div class="globalmenu">
+            <div class="global-menu">
 
                 <a href="index.html">
 
-                    <svg class="globalmenu-logo" viewBox="0 0 1024 1024">
+                    <svg class="global-menu-logo" viewBox="0 0 1024 1024">
                         <path d="M899.471 169.143V32L124.531 169.143H899.471Z"/>
                         <path d="M757.135 717.714L899.471 224H124.531V498.286H266.867L124.531 992H899.471V717.714H757.135Z"/>
                     </svg>
@@ -19,7 +19,7 @@ class ComponentGlobalMenu extends HTMLElement{
 
                 <a href="javascript:history.back()">
 
-                    <svg class="globalmenu-chevron" viewBox="0 0 1024 1024">
+                    <svg class="global-menu-chevron" viewBox="0 0 1024 1024">
                         <path d="M712.835 823.161L401.672 511.997L712.835 200.834L652.495 140.494L280.992 511.997L652.495 883.501L712.835 823.161Z"/>
                     </svg>
 
@@ -27,7 +27,7 @@ class ComponentGlobalMenu extends HTMLElement{
 
                 <a href="javascript:history.forward()">
 
-                    <svg class="globalmenu-chevron" viewBox="0 0 1024 1024">
+                    <svg class="global-menu-chevron" viewBox="0 0 1024 1024">
                         <path d="M311.168 200.84L622.331 512.003L311.168 823.167L371.508 883.507L743.011 512.003L371.508 140.5L311.168 200.84Z"/>
                     </svg>
 
